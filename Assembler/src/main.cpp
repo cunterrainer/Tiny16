f513fcfd390e1c7bdcd9366517c2da800104f077
cunterrainer <vyx3327@web.de>
2025-07-23 17:31:09 +0200

    Added line parsing

--- Assembler/src/main.cpp
+++ Assembler/src/main.cpp
@@ -51,35 +51,118 @@ std::string Trim(std::string_view str)
     return std::string(start, end);
 }
 
-
-void ParseSourceCode(const std::vector<std::string>& lines)
+struct Instruction
 {
-    for (auto line : lines)
-    {
-        line = Trim(line);
-        if (line.empty()) continue;
+    size_t lineNumber;
+    std::string label;
+    std::string opcode;
+    std::string lhs;
+    std::string rhs;
+};
 
+
+
+std::vector<std::string> Tokenize(const std::string& line)
+{
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream stream(line);
+
+    while (stream >> token)
+    {
+        if (token.ends_with(','))
+        {
+            token.pop_back();
+            tokens.push_back(token);
+        }
+        else
+        {
+            tokens.push_back(token);
+        }
+    }
+    return tokens;
+}
+
+
+std::optional<Instruction> ParseLine(std::string line, size_t lineNumber)
+{
+    // Strip comment
+    const size_t commentPos = line.find('#');
+    if (commentPos != std::string_view::npos)
+        line = line.substr(0, commentPos);
+
+    line = Trim(line);
+    if (line.empty())
+        return std::nullopt;
+
+    std::vector<std::string> tokens = Tokenize(line);
+    if (tokens.empty()) return std::nullopt;
+
+    Instruction instr;
+    instr.lineNumber = lineNumber;
+
+    if (tokens.begin()->ends_with(':'))
+    {
+        instr.label = tokens.begin()->substr(0, tokens.begin()->size() - 1);
+
+        if (tokens.size() == 1)
+        {
+            return instr;
+        }
+        else
+        {
+            std::cerr << std::format("Error: token found after Label: '{}', Line {}", instr.label, instr.lineNumber) << std::endl;
+            return std::nullopt;
+        }
+    }
+
+    instr.opcode = tokens[0];
+
+    if (tokens.size() > 1)
+        instr.lhs = tokens[1];
+    if (tokens.size() > 2)
+        instr.rhs = tokens[2];
+
+    return instr;
+}
+
+
+std::vector<Instruction> ParseSourceCode(const std::vector<std::string>& lines)
+{
+    std::vector<Instruction> instructions;
+
+    for (size_t i = 0; i < lines.size(); i++)
+    {
         // Lowercase in-place
+        std::string line = lines[i];
         std::ranges::transform(line, line.begin(), [](unsigned char c) { return std::tolower(c); });
 
-        // Comment
-        if (line[0] == ';') continue;
-
-
-
+        std::optional<Instruction> instr = ParseLine(line, i);
         std::cout << line << std::endl;
+
+        if (!instr.has_value())
+            continue;
+        instructions.push_back(instr.value());
+#ifndef NDEBUG
+        std::cout << "Label: " << instr.value().label
+            << " | Opcode: " << instr.value().opcode
+            << " | Op1: " << instr.value().lhs
+            << " | Op2: " << instr.value().rhs << "\n";
+#endif
     }
+
+    return instructions;
 }
 
 
 int main()
 {
-    std::optional<std::vector<std::string>> sourceLines = ReadFile("examples/example1.s");
+    std::optional<std::vector<std::string>> sourceLines = ReadFile("examples/example2.s");
     if (!sourceLines)
         return 1;
 
-    ParseSourceCode(sourceLines.value());
-
+    std::vector<Instruction> instructions = ParseSourceCode(sourceLines.value());
+    
     std::cin.get();
     return 0;
 }
