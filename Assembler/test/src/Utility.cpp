666848e450e69165c38b84cbf2a4dea01f14f752
cunterrainer <vyx3327@web.de>
2025-07-23 18:34:04 +0200

    Added Trim() tests

--- Assembler/test/src/Utility.cpp
+++ Assembler/test/src/Utility.cpp
@@ -4,5 +4,66 @@
 
 TEST_CASE("Testing Trim()")
 {
-    CHECK(Trim("   a   ") == "a");
+    CHECK(""     == Trim(""));
+    CHECK(""     == Trim("    "));
+    CHECK("Test" == Trim("Test"));
+
+    SUBCASE("Left trim test")
+    {
+        SUBCASE("Without space in word")
+        {
+            CHECK("Test" == Trim(" Test"));
+            CHECK("Test" == Trim("   Test"));
+            CHECK("Test" == Trim("           Test"));
+        }
+
+        SUBCASE("With space in word")
+        {
+            CHECK("Te   st"     == Trim(" Te   st"));
+            CHECK("Te    st"    == Trim("   Te    st"));
+            CHECK("Te       st" == Trim("           Te       st"));
+        }
+    }
+
+    SUBCASE("Right trim test")
+    {
+        SUBCASE("Without space in word")
+        {
+            CHECK("Test" == Trim("Test "));
+            CHECK("Test" == Trim("Test   "));
+            CHECK("Test" == Trim("Test           "));
+        }
+
+        SUBCASE("With space in word")
+        {
+            CHECK("Te   st"     == Trim("Te   st "));
+            CHECK("Te    st"    == Trim("Te    st   "));
+            CHECK("Te       st" == Trim("Te       st           "));
+        }
+    }
+
+    SUBCASE("Right and left trim test")
+    {
+        SUBCASE("Without space in word")
+        {
+            CHECK("Test" == Trim(" Test "));
+            CHECK("Test" == Trim("   Test   "));
+            CHECK("Test" == Trim("           Test           "));
+
+            CHECK("Test" == Trim("   Test "));
+            CHECK("Test" == Trim("      Test   "));
+            CHECK("Test" == Trim("             Test         "));
+        }
+
+        SUBCASE("With space in word")
+        {
+            CHECK("Te   st"     == Trim(" Te   st "));
+            CHECK("Te    st"    == Trim("   Te    st   "));
+            CHECK("Te       st" == Trim("           Te       st           "));
+
+            CHECK("Te       st" == Trim("   Te       st "));
+            CHECK("Te       st" == Trim("      Te       st   "));
+            CHECK("Te       st" == Trim("             Te       st         "));
+        }
+    }
 }
