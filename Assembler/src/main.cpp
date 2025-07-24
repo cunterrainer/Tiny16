6f02a0fdcfe34a7fa08ee64c5b849730433a618f
pyvyx <vyx3327@web.de>
2025-07-24 03:48:53 +0200

    Added instruction checking

--- Assembler/src/main.cpp
+++ Assembler/src/main.cpp
@@ -1,17 +1,30 @@
 #include <vector>
+#include <format>
 #include <string>
 #include <optional>
+#include <iostream>
 #include <string_view>
 
 #include "Parser.hpp"
 #include "Utility.hpp"
+#include "Validator.hpp"
 
 int main()
 {
-    std::optional<std::vector<std::string>> sourceLines = ReadFile("examples/example2.s");
+    const std::string file = "Assembler/examples/example2.s";
+    std::optional<std::vector<std::string>> sourceLines = ReadFile(file);
     if (!sourceLines)
         return 1;
 
     std::vector<Instruction> instructions = ParseSourceCode(sourceLines.value());
+    for (const auto& instr : instructions)
+    {
+        const ValidationResult result = ValidateInstruction(instr);
+        if (!result.valid)
+        {
+            std::cerr << result.errorMsg << std::format("\nLine: {}, File: {}", instr.lineNumber, file) << std::endl;
+            return -1;
+        }
+    }
     return 0;
 }
