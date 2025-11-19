f1325009922a210922d237dbbce2b3fcb0db7d5f
cunterrainer <vyx3327@web.de>
2025-11-19 11:14:35 +0100

    Seperated Disassembler into header and cpp file & added operand parsing

---
+++ Emulator/src/Disassembler.cpp
@@ -0,0 +1,74 @@
+#include <vector>
+#include <string>
+#include <cstdint>
+#include <unordered_map>
+
+#include "../../Assembler/lib/src/Assembler.hpp"
+
+#include "Log.hpp"
+#include "Disassembler.hpp"
+
+std::string Disassembler::DisassembleOperands(OpcodeMC opcode, const std::vector<std::uint8_t>& machineCode, size_t index) const
+{
+    switch (opcode)
+    {
+    case OpcodeMC::LOAD:
+    {
+        const std::uint16_t immediateValue = (machineCode.at(index + 2) << 8) | machineCode.at(index + 1);
+        return std::format(" $0x{:04X}, R{}", immediateValue, machineCode.at(index + 3));
+    }
+    case OpcodeMC::MOV_IMM_TO_REG:
+    case OpcodeMC::ADD_IMM_TO_REG:
+    case OpcodeMC::SUB_IMM_TO_REG:
+    case OpcodeMC::CMP_IMM_TO_REG:
+    {
+        const std::uint16_t immediateValue = (machineCode.at(index + 2) << 8) | machineCode.at(index + 1);
+        return std::format(" $0x{:X}, R{}", immediateValue, machineCode.at(index + 3));
+    }
+    case OpcodeMC::MOV_REG_TO_REG:
+    case OpcodeMC::ADD_REG_TO_REG:
+    case OpcodeMC::SUB_REG_TO_REG:
+    case OpcodeMC::CMP_REG_TO_REG:
+        return std::format(" R{}, R{}", machineCode.at(index + 1), machineCode.at(index + 2));
+    case OpcodeMC::JE_REG:
+    case OpcodeMC::JMP_REG:
+        return std::format(" R{}", machineCode.at(index + 1));
+    case OpcodeMC::JE_LABEL:
+    case OpcodeMC::JMP_LABEL:
+    {
+        const std::uint16_t immediateValue = (machineCode.at(index + 2) << 8) | machineCode.at(index + 1);
+        return std::format(" $0x{:04X}", immediateValue);
+    }
+    case OpcodeMC::STORE:
+    {
+        const std::uint16_t immediateValue = (machineCode.at(index + 3) << 8) | machineCode.at(index + 2);
+        return std::format(" R{}, $0x{:04X}", machineCode.at(index + 1), immediateValue);
+    }
+    case OpcodeMC::HLT:
+        return std::string();
+    }
+    return " ???, ???";
+}
+
+
+Disassembler::Disassembler(const std::vector<std::uint8_t>& machineCode)
+{
+    m_SourceCode.reserve(machineCode.size());
+
+    for (size_t i = 0; i < machineCode.size();)
+    {
+        try
+        {
+            const OpcodeMC opcode = static_cast<OpcodeMC>(machineCode[i]);
+            const std::string opcodeStr(m_OpcodeToStringMap.at(opcode));
+            const std::string operands = DisassembleOperands(opcode, machineCode, i);
+
+            m_SourceCode.emplace_back(opcodeStr + operands);
+            i += s_InstructionIRSizeMap.at(opcode);
+        }
+        catch (const std::out_of_range&)
+        {
+            ERR("Failed to disassemble source code, instruction: {} | 0x{:X}, at address: {} | 0x{:04X}", (int)machineCode[i], (int)machineCode[i], i, i);
+        }
+    }
+}
--- Emulator/src/Disassembler.hpp
+++ Emulator/src/Disassembler.hpp
@@ -2,12 +2,11 @@
 #define DISASSEMBLER_H
 
 #include <vector>
+#include <string>
 #include <cstdint>
-#include <stdexcept>
+#include <unordered_map>
 
 #include "../../Assembler/lib/src/Assembler.hpp"
-
-#include "Log.hpp"
 
 class Disassembler
 {
@@ -15,44 +14,29 @@ private:
     std::vector<std::string> m_SourceCode;
 
     const std::unordered_map<OpcodeMC, std::string_view> m_OpcodeToStringMap = {
-        { OpcodeIR::MOV_IMM_TO_REG, "MOV" },
-        { OpcodeIR::ADD_IMM_TO_REG, "ADD" },
-        { OpcodeIR::SUB_IMM_TO_REG, "SUB" },
-        { OpcodeIR::CMP_IMM_TO_REG, "CMP" },
-        { OpcodeIR::MOV_REG_TO_REG, "MOV" },
-        { OpcodeIR::ADD_REG_TO_REG, "ADD" },
-        { OpcodeIR::SUB_REG_TO_REG, "SUB" },
-        { OpcodeIR::CMP_REG_TO_REG, "CMP" },
-        { OpcodeIR::JMP_REG       , "JMP" },
-        { OpcodeIR::JMP_LABEL     , "JMP" },
-        { OpcodeIR::JE_REG        , "JE" },
-        { OpcodeIR::JE_LABEL      , "JE" },
-        { OpcodeIR::HLT           , "HTL" },
-        { OpcodeIR::LOAD          , "LOAD" },
-        { OpcodeIR::STORE         , "STORE" },
+        { OpcodeMC::MOV_IMM_TO_REG, "MOV" },
+        { OpcodeMC::ADD_IMM_TO_REG, "ADD" },
+        { OpcodeMC::SUB_IMM_TO_REG, "SUB" },
+        { OpcodeMC::CMP_IMM_TO_REG, "CMP" },
+        { OpcodeMC::MOV_REG_TO_REG, "MOV" },
+        { OpcodeMC::ADD_REG_TO_REG, "ADD" },
+        { OpcodeMC::SUB_REG_TO_REG, "SUB" },
+        { OpcodeMC::CMP_REG_TO_REG, "CMP" },
+        { OpcodeMC::JMP_REG       , "JMP" },
+        { OpcodeMC::JMP_LABEL     , "JMP" },
+        { OpcodeMC::JE_REG        , "JE " },
+        { OpcodeMC::JE_LABEL      , "JE " },
+        { OpcodeMC::HLT           , "HTL" },
+        { OpcodeMC::LOAD          , "LOAD" },
+        { OpcodeMC::STORE         , "STORE" },
     };
+private:
+    std::string DisassembleOperands(OpcodeMC opcode, const std::vector<std::uint8_t>& machineCode, size_t index) const;
 public:
-    explicit Disassembler(const std::vector<std::uint8_t>& machineCode)
-    {
-        m_SourceCode.reserve(machineCode.size());
-
-        for (size_t i = 0; i < machineCode.size();)
-        {
-            try
-            {
-                const OpcodeMC opcode = static_cast<OpcodeMC>(machineCode[i]);
-                m_SourceCode.emplace_back(m_OpcodeToStringMap.at(opcode));
-                i += s_InstructionIRSizeMap.at(opcode);
-            }
-            catch (const std::out_of_range& e)
-            {
-                ERR("Failed to disassemble source code, instruction: {} | 0x{:X}, at address: {} | 0x{:04X}", (int)machineCode[i], (int)machineCode[i], i, i);
-            }
-        }
-    }
+    explicit Disassembler(const std::vector<std::uint8_t>& machineCode);
 
 
-    const std::vector<std::string>& GetSourceInstructions() const noexcept
+    inline const std::vector<std::string>& GetSourceInstructions() const noexcept
     {
         return m_SourceCode;
     }
