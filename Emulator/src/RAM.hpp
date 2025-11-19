8c674f2d1fbacb3fabb5b1420d3b518ea6712bcb
cunterrainer <vyx3327@web.de>
2025-11-19 15:44:56 +0100

    Added RAM class

---
+++ Emulator/src/RAM.hpp
@@ -0,0 +1,25 @@
+#ifndef RAM_HPP
+#define RAM_HPP
+
+#include <vector>
+#include <limits>
+#include <cstdint>
+
+class RAM
+{
+private:
+    std::vector<std::uint16_t> m_Memory = std::vector<std::uint16_t>(std::numeric_limits<std::uint16_t>::max() + 1 /* +1 for the 65536 byte */, 0);
+public:
+    inline std::uint16_t GetMemory(std::uint16_t address) const noexcept
+    {
+        return m_Memory[address];
+    }
+
+    inline size_t GetSize() const noexcept
+    {
+        return m_Memory.size();
+    }
+};
+
+
+#endif // RAM_HPP
